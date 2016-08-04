cb3db356d51ca9ab25c24595646df3ddb2cfe7de
Brian Hague <bhague1281@gmail.com>
2016-08-04 13:54:49 -0700

    setup user data and authorization

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,15 @@
 class MainController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     redirect_to leaderboard_path if current_user
   end
 
   def leaderboard
-    @users = User.where(cohort: current_user.cohort).joins(:event_snapshots)
+    if current_user
+      @users = User.where(cohort: current_user.cohort).joins(:event_snapshots)
+    else
+      @users = []
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     :skip => [:sessions]
 
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
