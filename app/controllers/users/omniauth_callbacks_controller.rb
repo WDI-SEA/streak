c86aa710a6877b4d576e600d153b062325bb26a5
Brian Hague <bhague1281@gmail.com>
2016-08-08 12:01:54 -0700

    added some display changes, GA styling from GArnet/GA website

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
-    if !@user.active?
-      flash[:alert] = 'Thank you for signing up. You\'ll receive an email once approved.'
-    end
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => 'Github') if is_navigational_format?
+      set_flash_message(:notice, :success, :kind => 'Github') if is_navigational_format? && !@user.active?
     else
       session['devise.github_data'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
